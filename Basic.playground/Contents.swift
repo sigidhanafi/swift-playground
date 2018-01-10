6ca260a395f498949df3abbb5b7369ade147156c
sigidhanafi <sigidhanafi@gmail.com>
2018-01-10 22:36:45 +0700

    For loop dictionary

--- Basic.playground/Contents.swift
+++ Basic.playground/Contents.swift
@@ -21,7 +21,7 @@ var emptyArray = [String]()
 
 let indifidualScores = [10, 20, 50, 45, 70, 80, 60]
 
-// for loop
+// for loop array
 var teamScore = 0
 for score in indifidualScores {
     if score > 50 {
@@ -61,3 +61,20 @@ switch vegetable {
     default:
         print("Everything tastes good in soup.")
 }
+
+// for loop dictionary
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 9, 11],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if (number > largest) {
+            largest = number
+        }
+    }
+}
+print("Largest number \(largest)")
